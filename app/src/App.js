f8266427aa502d215928addad9aca6b8258d7147
octavio_vercetti <octavio@vercetti.dev>
2021-12-08 20:00:54 +0100

    Deploy candy machine to devnet

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import "./App.css"
 import twitterLogo from "./assets/twitter-logo.svg"
 
@@ -7,6 +7,7 @@ const TWITTER_HANDLE = "_buildspace"
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`
 
 const App = () => {
+  const [walletAddress, setWalletAddress] = useState(null)
   const checkIfWalletIsConnected = async () => {
     try {
       const { solana } = window
@@ -15,6 +16,7 @@ const App = () => {
         if (solana.isPhantom) {
           console.log("Phantom wallet found!")
           const response = await solana.connect({ onlyIfTrusted: true })
+          setWalletAddress(response.publicKey.toString())
           console.log(
             "Connected with Public Key:",
             response.publicKey.toString()
@@ -27,6 +29,23 @@ const App = () => {
       console.error(error)
     }
   }
+  const connectWallet = async () => {
+    const { solana } = window
+
+    if (solana) {
+      const response = await solana.connect()
+      console.log("Connected with Public Key:", response.publicKey.toString())
+      setWalletAddress(response.publicKey.toString())
+    }
+  }
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  )
   useEffect(() => {
     const onLoad = async () => {
       await checkIfWalletIsConnected()
@@ -40,6 +59,7 @@ const App = () => {
         <div className="header-container">
           <p className="header">🍭 Candy Drop</p>
           <p className="sub-text">NFT drop machine with fair mint</p>
+          {!walletAddress && renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
