1740c0bfb475d9ffb8649dd57c781366daf11721
octavio_vercetti <octavio@vercetti.dev>
2021-12-08 13:54:03 +0100

    log phantom wallet public key

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,39 @@
-import React from 'react';
-import './App.css';
-import twitterLogo from './assets/twitter-logo.svg';
+import React, { useEffect } from "react"
+import "./App.css"
+import twitterLogo from "./assets/twitter-logo.svg"
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const TWITTER_HANDLE = "_buildspace"
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`
 
 const App = () => {
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log("Phantom wallet found!")
+          const response = await solana.connect({ onlyIfTrusted: true })
+          console.log(
+            "Connected with Public Key:",
+            response.publicKey.toString()
+          )
+        }
+      } else {
+        alert("Solana object not found! Get a Phantom Wallet 👻")
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected()
+    }
+    window.addEventListener("load", onLoad)
+    return () => window.removeEventListener("load", onLoad)
+  }, [])
   return (
     <div className="App">
       <div className="container">
@@ -25,7 +52,7 @@ const App = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
